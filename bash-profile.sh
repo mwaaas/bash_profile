75b87b87fc9ab0f879157313a87f3f6777af1bdb
dorcas <dorcas@paul-peru.mobile.local>
2018-03-12 13:07:50 +0300

    added commands for exporting aws_work

--- bash-profile.sh
+++ bash-profile.sh
@@ -1,1 +1,6 @@
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+fi
+
 alias c="docker-compose"
+alias aws_work="export AWS_ACCESS_KEY_ID=$WORK_AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$WORK_AWS_SECRET_ACCESS_KEY"
