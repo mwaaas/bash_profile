742648dc5fdf8e06de6397011da016bd02effbbd
francismwangi <francismwangi152@gmail.com>
2018-03-12 22:09:30 +0300

    added another command for docker-compose up

--- bash-profile.sh
+++ bash-profile.sh
@@ -3,4 +3,5 @@ if [ -f ~/.bashrc ]; then
 fi
 
 alias c="docker-compose"
+alias up="docker-compose up"
 alias aws_work="export AWS_ACCESS_KEY_ID=$WORK_AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$WORK_AWS_SECRET_ACCESS_KEY"
