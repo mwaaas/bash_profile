9841371ff274b2cbd6f7d7c58f2e7c98affa1e8f
francismwangi <francismwangi152@gmail.com>
2018-03-14 05:46:34 +0300

    added more aliases

--- bash-profile.sh
+++ bash-profile.sh
@@ -2,10 +2,6 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-alias c="docker-compose"
-alias up="docker-compose up"
-alias aws_work="export AWS_ACCESS_KEY_ID=$WORK_AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$WORK_AWS_SECRET_ACCESS_KEY"
-
 function _makefile_targets {
     local curr_arg;
     local targets;
@@ -25,3 +21,9 @@ function _makefile_targets {
     COMPREPLY=( $(compgen -W "${targets[@]}" -- $curr_arg ) );
 }
 complete -F _makefile_targets make
+
+alias c="docker-compose"
+alias up="docker-compose up"
+alias aws_work="export AWS_ACCESS_KEY_ID=$WORK_AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$WORK_AWS_SECRET_ACCESS_KEY"
+alias docker_stop="docker stop $(docker ps -aq)"
+alias docker_rm="docker rm $(docker ps -aq)"
