55d8a77643370bbdf005f117196d2036b4f0b940
mwaaas <francismwangi152@gmail.com>
2018-04-19 10:41:38 +0300

    added more commands

--- bash-profile.sh
+++ bash-profile.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
@@ -21,10 +22,12 @@ function _makefile_targets {
     COMPREPLY=( $(compgen -W "${targets[@]}" -- $curr_arg ) );
 }
 complete -F _makefile_targets make
-
+export AWS_REGION=eu-west-1
 alias c="docker-compose"
 alias up="docker-compose up"
 alias aws_work="export AWS_ACCESS_KEY_ID=$WORK_AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$WORK_AWS_SECRET_ACCESS_KEY"
-alias docker_stop="docker stop $(docker ps -aq)"
-alias docker_rm="docker rm $(docker ps -aq)"
+alias docker_stop="docker stop $(`docker ps -aq`)"
+alias docker_rm="docker rm $(`docker ps -aq`)"
+alias clean_pycharm_debug="docker rm $(docker ps -a --filter="label=com.jetbrains.pycharm_helpers.version" -q)"
+alias awsSsh="docker run -it -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  -v ~/.ssh:/root/.ssh/ -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e AWS_REGION=${AWS_REGION} mwaaas/aws_ssh:latest "
 aws_work
