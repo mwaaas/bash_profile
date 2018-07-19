72781d16a74fe5c9c4d2edcaca25b323da23eca7
mwas <francismwangi152@gmail.com>
2018-07-19 07:32:43 +0300

    added rvm scripts

--- bash-profile.sh
+++ bash-profile.sh
@@ -3,6 +3,9 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
 function _makefile_targets {
     local curr_arg;
     local targets;
